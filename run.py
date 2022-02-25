822a8edabcca2b6be9d6da04a2fd50e6b199a60c
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2022-02-25 21:21:00 +0100

    fix issues

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 from config import config_from_env, config_from_dict, ConfigurationSet
 from flask.logging import default_handler
+from gevent.pywsgi import WSGIServer
 import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
 import pandas as pd
@@ -8,14 +9,26 @@ import logging
 
 
 DEFAULT_CONFIG = {
+    # information about the server
     "app.host": "0.0.0.0",
     "app.port": 8080,
     "app.debug": False,
+
+    # address of mqtt broker
     "mqtt.host": "localhost",
     "mqtt.port": 1883,
+
+    # log level for debugging
     "log_level": "WARNING",
+
+    # store system topic messages
     "sys_on": False,
-    "max_entries": 100
+
+    # maximum number of records to be stored in memory
+    "max_entries": 100,
+
+    # maximum number of entries to display in the web interface
+    "max_cons_rows": 50
 }
 
 PREFIX = "MQTT_LISTEN"
@@ -47,8 +60,9 @@ if __name__ == "__main__":
         if (cfg.sys_on == "False") and msg.topic.startswith("$SYS"):
             return
         if results.shape[0] >= cfg.max_entries:
-            results = results.iloc[3:, :]
-        results.loc[results.shape[0]] = [msg.timestamp, msg.topic, msg.payload]
+            results = results.iloc[1:, :]
+        new_record = pd.DataFrame.from_records([{"time": msg.timestamp, "topic": msg.topic, "payload": msg.payload}])
+        results = pd.concat([results, new_record], ignore_index=True)
 
 
     client = mqtt.Client("MQTT_Listener", clean_session=True)
@@ -71,11 +85,15 @@ if __name__ == "__main__":
 
     @app.route("/data", methods=["GET"])
     def get_results():
-        result_df = results.tail(50)
+        result_df = results.tail(cfg.max_cons_rows)
         result_list = []
         for idx, row in result_df.iterrows():
             date = round(row["time"])
             result_list.append({"time": date, "topic": row["topic"], "payload": str(row["payload"]).replace("b\'", "").replace("\'", "")})
         return jsonify(result_list)
 
-    app.run(host=cfg["app"]["host"], port=cfg["app"]["port"], debug=cfg["app"]["debug"], use_reloader=False)
+    if cfg["app"]["debug"] == True:
+        app.run(host=cfg["app"]["host"], port=cfg["app"]["port"], debug=cfg["app"]["debug"], use_reloader=False)
+    else:
+        http_server = WSGIServer((cfg["app"]["host"], cfg["app"]["port"]), app)
+        http_server.serve_forever()
